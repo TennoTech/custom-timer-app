906f3c6f8f968c3fe0842b67dcced5fab5fb67e0
Roman R <sakurasou2270@gmail.com>
2026-06-29 00:27:51 -0500

    updated timer logic & refactored window class

--- meter.py
+++ meter.py
@@ -8,3 +8,25 @@
 #         root.after(1000, self.update_meter)
 #     else:
 #         meter.configure(subtext="Timer Complete")F
+
+# percentage = 100
+
+
+# meter = ttkb.Meter(
+#     metersize=200,  # Size of the meter
+#     padding=20,  # distance between window edges
+#     meterthickness=5,  # thickness of the meter dial
+#     stripethickness=5,  # thickness of discrete stripes,0=Continous
+#     bootstyle="info",  # allows you to apply pre-defined styles like primary,danger,success etc
+#     metertype="semi",  # Semicircle ,"full" =circular
+#     amounttotal=100,  # max value of scale =100
+#     amountused=percentage,  # current value
+#     subtext="Timer",
+#     subtextstyle="info",  # allows you to apply pre-defined styles like primary,danger,success etc
+#     subtextfont="-size 10 -weight bold",
+#     interactive=False,  # you can change meter position by mouse
+# )
+
+# meter.pack()
+
+# update_meter()
--- timer_component.py
+++ timer_component.py
@@ -6,83 +6,67 @@ import ttkbootstrap as ttkb
 from ttkbootstrap.constants import *
 from tool_tip import DynamicToolTip
 
-root = ttkb.Window()
-# root.overrideredirect(True)
-# root.title("Always on Top")
-window_width, window_height = 500, 400
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
-center_x = int(screen_width / 2 - window_width / 2)
-center_y = int(screen_height / 2 - window_height / 2) - 100
-root.geometry(f"{window_width}x{window_height}+{center_x}+{center_y}")
-root.attributes("-topmost", True)
 
-percentage = 100
+class TimerWindow:
+    def __init__(self):
+        self.root = ttkb.Window(themename="darkly")
+        self.window_width, self.window_height = 500, 400
+
+        self._setup_window()
+        self._create_widgets()
+
+        self.root.update_idletasks()
+        self.root.mainloop()
+
+    def _setup_window(self):
+        screenwidth = self.root.winfo_screenwidth()
+        screenheight = self.root.winfo_screenheight()
+        x = int((screenwidth - self.window_width) / 2)
+        y = int((screenheight - self.window_height) / 2) - 100
+
+        self.root.geometry(f"{self.window_width}x{self.window_height}+{x}+{y}")
+        self.root.attributes("-topmost", True)
+        # self.root.overrideredirect(True)
+
+    def _create_widgets(self):
+        #!########## Title ###########
+        ttkb.Label(
+            self.root, bootstyle="info", text="Set Timer", font=("Arial", 16)
+        ).pack(pady=(10, 0))
+
+        #!########## Separator ###########
+        ttkb.Separator(self.root, bootstyle="info", orient=HORIZONTAL).pack(
+            fill="x", pady=(5, 0), padx=20
+        )
+
+        #!########## Input ###########
+        self.user_input = ttkb.Entry(self.root, bootstyle="dark")
+        self.user_input.pack(pady=(25, 0))
+        self.input_validator_tip = DynamicToolTip(ttkb, self.user_input)
+        self.user_input.bind("<Return>", self.on_submit)
+
+        #!########## Action Button ###########
+        ttkb.Button(
+            self.root,
+            bootstyle="success",
+            text="Ready",
+            padding=5,
+            command=self.on_click,
+        ).pack(pady=10)
+
+    def on_submit(self, event=None):
+        val = self.user_input.get()
+        if val.isdigit():
+            #! calculate time || hour/minute/seconds
+            print(f"Success! Timer set to: {val}")
+        else:
+            print("Error: Please enter numbers only!")
+            self.input_validator_tip.show_error("Error: Please enter numbers only!")
+
+    def on_click(self):
+        #! Placeholder logic for now
+        print("Exiting Program")
+        self.root.destroy()
 
 
-def on_submit(event=None):
-    if user_input.get().isdigit():
-        timer_text = user_input.get()
-        print(f"Success! Timer set to: {timer_text}")
-        input_validator_tip.hide_error()  # Clean up any existing error
-    else:
-        # Instantly fires the tooltip right below the invalid input field
-        input_validator_tip.show_error("Please enter numbers only!")
-
-
-#! ############# Main Text #############
-main_text = ttkb.Label(
-    root,
-    bootstyle="info",
-    text="Set Timer",
-)
-main_text.pack(anchor="n", pady=(10, 0))
-
-#! ############# Separator #############
-separator = ttkb.Separator(
-    root,
-    bootstyle="info",
-    orient=HORIZONTAL,
-)
-separator.pack(fill="x", anchor="n", pady=(5, 0), padx=20)
-
-# from tkinter import messagebox
-
-#! ############# Button #############
-user_input = ttkb.Entry(
-    root,
-    bootstyle="dark",
-)
-user_input.pack(pady=(25, 0))
-user_input.bind("<Return>", on_submit)
-input_validator_tip = DynamicToolTip(user_input)
-
-#! ############# Button #############
-button = ttkb.Button(
-    root,
-    bootstyle="success",
-    text="Ready",
-    padding=5,
-)
-# button.pack()
-
-# meter = ttkb.Meter(
-#     metersize=200,  # Size of the meter
-#     padding=20,  # distance between window edges
-#     meterthickness=5,  # thickness of the meter dial
-#     stripethickness=5,  # thickness of discrete stripes,0=Continous
-#     bootstyle="info",  # allows you to apply pre-defined styles like primary,danger,success etc
-#     metertype="semi",  # Semicircle ,"full" =circular
-#     amounttotal=100,  # max value of scale =100
-#     amountused=percentage,  # current value
-#     subtext="Timer",
-#     subtextstyle="info",  # allows you to apply pre-defined styles like primary,danger,success etc
-#     subtextfont="-size 10 -weight bold",
-#     interactive=False,  # you can change meter position by mouse
-# )
-
-# meter.pack()
-
-# update_meter()
-
-root.mainloop()
+window = TimerWindow()
